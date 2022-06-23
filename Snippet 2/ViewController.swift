6ab189fae48195b5754d204cbbf73947a9d5a6a0
sebjvidal <seb.vidal@btinternet.com>
2022-06-23 19:36:11 +0100

    Initial Commit

---
+++ Snippet 2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Snippet 2
+//
+//  Created by Seb Vidal on 23/06/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
